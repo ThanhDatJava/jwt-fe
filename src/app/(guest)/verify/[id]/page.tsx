90a3f24a9f877530364c2a30d18da22d02099be9
ThanhDatJava <thanhdatjava@gmail.com>
2024-12-23 20:00:13 +0700

    #16

---
+++ src/app/(guest)/verify/[id]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import Verify from "@/components/auth/verify";
+
+const VerifyPage = ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  return (
+    <>
+      <Verify id={id} />
+    </>
+  );
+};
+
+export default VerifyPage;
--- src/app/(guest)/verify/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-const VerifyPage = () => {
-  return <div>VerifyPage</div>;
-};
-
-export default VerifyPage;
